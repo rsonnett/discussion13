38c41a1d40100a13f9a7f33a2952a5315e60bb05
rsonnett <156856325+rsonnett@users.noreply.github.com>
2024-04-09 17:46:31 -0400

    Add files via upload

---
+++ js/challenge1.js
@@ -0,0 +1,9 @@
+document.querySelector('#toggle').addEventListener("click", function());
+function myFunction(){
+    var x = document.getElementById('#emailBox')
+    if (x.style.display == "none"){
+        x.style.display == "block";
+    } else{
+        x.style.display = "none";
+    }
+}
---
+++ js/challenge3.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    let figcaptions = document.querySelectorAll('figcaption');
+    let secondFigcaption = figcaptions[1];
+    this.alert(document.querySelectorAll('figcaption')[1].innerHTML);
+});
